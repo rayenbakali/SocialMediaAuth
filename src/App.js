1c0e7396b06c5ded6a90a47e4a2d343c1b05123f
Rayenbakali <rayen.bakali@@gmail.com>
2023-03-15 14:36:24 +0100

    google-facebook-git-auth

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import "./App.css";
+import Home from "./Pages//Home";
+import Navbar from "./Components/Navbar";
+import Post from "./Pages/Post";
+import Login from "./Pages/Login";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const getUser = () => {
+      fetch("http://localhost:5000/auth/login/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+        },
+      })
+        .then((response) => {
+          if (response.status === 200) return response.json();
+          throw new Error("authentication has been failed!");
+        })
+        .then((resObject) => {
+          setUser(resObject.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getUser();
+  }, []);
+
+  console.log(user);
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar user={user} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/post/:id"
+            element={user ? <Post /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
